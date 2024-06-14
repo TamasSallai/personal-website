8daa0c155d2bc65d10a32568b6d13b28e9a33ce9
Tamas Sallai <tamas.sallai.hu@gmail.com>
2024-06-14 20:10:09 +0200

    dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config"
-import icon from "astro-icon"
-
 import react from "@astrojs/react"
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
