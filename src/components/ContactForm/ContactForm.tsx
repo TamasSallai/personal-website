95b373e982a4e2c6f29b067f12a76ab36059cd43
Tamas Sallai <tamas.sallai.hu@gmail.com>
2024-06-16 12:51:57 +0200

    dev

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -37,7 +37,7 @@ const ContactForm = () => {
         placeholder={t("form.name.placeholder")}
       />
 
-      <div className="flex gap-4">
+      <div className="flex flex-row gap-4">
         <InputGroup
           label={t("form.email.label")}
           name="email"
