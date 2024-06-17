6c80c7d316d9eaabe8489c301d4446a7cc9aa37a
Tamas Sallai <tamas.sallai.hu@gmail.com>
2024-06-17 11:26:01 +0200

    added node adapter for endpoint communication

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
+import node from "@astrojs/node"
 import icon from "astro-icon"
 
 // https://astro.build/config
@@ -13,4 +14,7 @@ export default defineConfig({
       prefixDefaultLocale: true,
     },
   },
+  adapter: node({
+    mode: "standalone",
+  }),
 })
